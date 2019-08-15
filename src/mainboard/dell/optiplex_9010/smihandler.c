fc9eb26a89754bbc3e201d3f97d3b6c6821a58bb
Michał Żygowski <michal.zygowski@3mdeb.com>
2019-08-15 15:08:58 +0200

    src/mainboard/dell/optiplex_9010/smihandler.c: add debugging to SMI
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/dell/optiplex_9010/smihandler.c
+++ src/mainboard/dell/optiplex_9010/smihandler.c
@@ -20,23 +20,48 @@
 #include <cpu/x86/smm.h>
 #include <southbridge/intel/bd82x6x/pch.h>
 #include <southbridge/intel/common/pmutil.h>
+#include <southbridge/intel/common/pmbase.h>
 
-static void mainboard_smi_handle_ec_sci(void)
+static void dump_pmbase(void)
 {
 
+	printk(BIOS_SPEW, "PM1_STS: %04x\n"
+			  "PM1_EN: %04x\n"
+			  "PM1_CNT: %08x\n"
+			  "PM1_TMR: %08x\n"
+			  "GPE0_STS: %08x\n"
+			  "GPE0_EN: %08x\n"
+			  "SMI_EN: %08x\n"
+			  "SMI_STS: %08x\n"
+			  "ALT_GP_SMI_EN: %04x\n"
+			  "ALT_GP_SMI_STS: %04x\n"
+			  "GPE_CNTL: %02x\n",
+			  read_pmbase16(PM1_STS), read_pmbase16(PM1_EN),
+			  read_pmbase32(PM1_CNT), read_pmbase32(PM1_TMR),
+			  read_pmbase32(GPE0_STS), read_pmbase32(GPE0_EN),
+			  read_pmbase32(SMI_EN), read_pmbase32(SMI_STS),
+			  read_pmbase16(ALT_GP_SMI_EN),
+			  read_pmbase16(ALT_GP_SMI_STS), read_pmbase8(GPE_CNTL)
+		);
 }
+
 
 void mainboard_smi_gpi(u32 gpi_sts)
 {
-	mainboard_smi_handle_ec_sci();
+	printk(BIOS_SPEW, "%s: gpi_sts: %08x\n", __func__, gpi_sts);
+	dump_pmbase();
 }
 
 int mainboard_smi_apmc(u8 data)
 {
 	switch (data) {
 	case APM_CNT_ACPI_ENABLE:
+		printk(BIOS_SPEW, "%s: APM CNT EN: %02x\n", __func__, data);
+		dump_pmbase();
 		break;
 	case APM_CNT_ACPI_DISABLE:
+		printk(BIOS_SPEW, "%s: APM CNT DIS: %02x\n", __func__, data);
+		dump_pmbase();
 		break;
 	default:
 		break;
@@ -46,6 +71,8 @@ int mainboard_smi_apmc(u8 data)
 
 void mainboard_smi_sleep(u8 slp_typ)
 {
+	printk(BIOS_SPEW, "%s: SMI sleep: %02x\n", __func__, slp_typ);
+	dump_pmbase();
 	if (slp_typ == 3) {
 
 	}
