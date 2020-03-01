dacc19ea31385c3a9b848b5f149839bc5c5219a0
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-03-01 22:35:03 +0100

    src/superio/smsc/sch5545: add ACPI tables and EMI interface
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/superio/smsc/sch5545/sch5545_early_init.c
+++ src/superio/smsc/sch5545/sch5545_early_init.c
@@ -1,8 +1,6 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2017 Alexander Couzens <lynxis@fe80.eu>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 of the License.
@@ -15,7 +13,6 @@
 
 
 #include <arch/io.h>
-#include <console/console.h>
 #include <device/pnp.h>
 #include <stdint.h>
 #include <device/pnp_def.h>
@@ -39,14 +36,9 @@ static void pnp_exit_conf_state(pnp_devfn_t dev)
  * pnp_devfn_t dev must be in conf state.
  * LDN LPC IF must be active.
  */
-static void set_iobase(pnp_devfn_t dev, uint16_t device_addr, uint32_t bar_addr)
+static void set_iobase(pnp_devfn_t dev, uint16_t device_addr, uint16_t bar_addr)
 {
-	uint8_t val;
 	uint16_t bar;
-	unsigned port = dev >> 8;
-
-	pnp_devfn_t lpcif = PNP_DEV(port, SCH5545_LDN_LPC_IF);
-	pnp_set_logical_device(lpcif);
 
 	/* set BAR
 	 * we have to flip the BAR due to different register layout:
@@ -55,13 +47,6 @@ static void set_iobase(pnp_devfn_t dev, uint16_t device_addr, uint32_t bar_addr)
 	 */
 	bar = ((bar_addr >> 8) & 0xff) | ((bar_addr & 0xff) << 8);
 	pnp_set_iobase(dev, device_addr + 2, bar);
-
-	/* set valid */
-	val = pnp_read_config(dev, device_addr + 1);
-	val |= 0x80;
-	pnp_write_config(dev, device_addr + 1, val);
-
-	pnp_set_logical_device(dev);
 }
 
 /*
@@ -71,14 +56,10 @@ static void set_iobase(pnp_devfn_t dev, uint16_t device_addr, uint32_t bar_addr)
  */
 static void set_irq(pnp_devfn_t dev, uint8_t irq_device, unsigned irq)
 {
-	unsigned port = dev >> 8;
 	if (irq > 15)
 		return;
 
-	pnp_devfn_t lpcif = PNP_DEV(port, SCH5545_LDN_LPC_IF);
-	pnp_set_logical_device(lpcif);
 	pnp_write_config(dev, SCH5545_IRQ_BASE + irq, irq_device);
-	pnp_set_logical_device(dev);
 }
 
 /* sch5545 has 2 LEDs which are accessed via color (1 bit),
@@ -88,6 +69,7 @@ static void set_irq(pnp_devfn_t dev, uint8_t irq_device, unsigned irq)
 void sch5545_set_led(unsigned runtime_reg_base, unsigned color, uint16_t blink)
 {
 	uint8_t val = blink & SCH5545_LED_BLINK_MASK;
+	val |= SCH5545_LED_CODE_FETCH;
 	if (color)
 		val |= SCH5545_LED_COLOR_GREEN;
 	outb(val, runtime_reg_base + SCH5545_RR_LED);
@@ -97,30 +79,83 @@ void sch5545_early_init(unsigned port)
 {
 	pnp_devfn_t dev;
 
+	/* Enable SERIRQ */
+	dev = PNP_DEV(port, SCH5545_LDN_GCONF);
+	pnp_enter_conf_state(dev);
+	pnp_set_logical_device(dev);
+	pnp_write_config(dev, 0x24, pnp_read_config(dev, 0x24) | 0x04);
+
+
 	/* enable lpc if */
 	dev = PNP_DEV(port, SCH5545_LDN_LPC_IF);
-	pnp_enter_conf_state(dev);
-
 	pnp_set_logical_device(dev);
 	pnp_set_enable(dev, 1);
+	/* Set LPC BAR mask */
+	pnp_write_config(dev, SCH5545_BAR_LPC_IF, 0x01);
+	/* BAR valid, Frame/LDN = 0xc */
+	pnp_write_config(dev, SCH5545_BAR_LPC_IF + 1,
+			 SCH5545_LDN_LPC_IF | 0x80);
+	set_iobase(dev, SCH5545_BAR_LPC_IF, port);
 
-	/* map runtime register */
+	/* Enable runtime registers */
+
+	/* The Runtime Registers BAR is 0x40 long */
+	pnp_write_config(dev, SCH5545_BAR_RUNTIME_REG, 0x3f);
+	/* BAR valid, Frame/LDN = 0xa */
+	pnp_write_config(dev, SCH5545_BAR_RUNTIME_REG + 1,
+			 SCH5545_LDN_RR | 0x80);
+
+	/* map runtime registers */
 	set_iobase(dev, SCH5545_BAR_RUNTIME_REG, SCH5545_RUNTIME_REG_BASE);
-
-	/* configure serial 1 / UART 1 */
-	dev = PNP_DEV(port, SCH5545_LDN_UART1);
+	dev = PNP_DEV(port, SCH5545_LDN_RR);
 	pnp_set_logical_device(dev);
 	pnp_set_enable(dev, 1);
-	pnp_write_config(dev, SCH5545_CONFIG_SELECT, SCH5545_UART_POWER_VCC);
-	set_iobase(dev, SCH5545_BAR_UART1, CONFIG_TTYS0_BASE);
-	set_irq(dev, SCH5545_IRQ_UART1, 4);
 
-	/* set LED yellow color */
-	sch5545_set_led(SCH5545_RUNTIME_REG_BASE, SCH5545_LED_COLOR_YELLOW,
+	/* Clear pending adn enable PMEs */
+	outb(0x01, SCH5545_RUNTIME_REG_BASE);
+	outb(0x01, SCH5545_RUNTIME_REG_BASE + 1);
+
+	/* set LED color and indicate BIOS has reached code fetch phase */
+	sch5545_set_led(SCH5545_RUNTIME_REG_BASE, SCH5545_LED_COLOR_GREEN,
 			SCH5545_LED_BLINK_ON);
 
+	/* Configure EMI */
 	dev = PNP_DEV(port, SCH5545_LDN_LPC_IF);
 	pnp_set_logical_device(dev);
+	/* EMI BAR has 11 registers, but vendor sets the mask to 0xf */
+	pnp_write_config(dev, SCH5545_BAR_EM_IF, 0x0f);
+	/* BAR valid, Frame/LDN = 0x00 */
+	pnp_write_config(dev, SCH5545_BAR_EM_IF + 1, SCH5545_LDN_EMI | 0x80);
+	set_iobase(dev, SCH5545_BAR_EM_IF, SCH5545_EMI_BASE);
+
+	pnp_exit_conf_state(dev);
+}
+
+void sch5545_enable_uart(unsigned port, unsigned uart_no)
+{
+	pnp_devfn_t dev;
+
+	if (uart_no > 1)
+		return;
+
+	/* configure serial 1 / UART 1 */
+	dev = PNP_DEV(port, SCH5545_LDN_LPC_IF);
+	pnp_set_logical_device(dev);
+	/* Set UART BAR mask to 0x07 (8 registers) */
+	pnp_write_config(dev, SCH5545_BAR_UART1 + (4 * uart_no), 0x07);
+	/* Set BAR valid, Frame/LDN = UART1/2 LDN 0x07/0x08 */
+	pnp_write_config(dev, SCH5545_BAR_UART1 + (4 * uart_no) + 1,
+			 (SCH5545_LDN_UART1 + uart_no) | 0x80);
+	set_iobase(dev, SCH5545_BAR_UART1 + (4 * uart_no),
+		   (uart_no == 1) ? 0x2f8 : 0x3f8);
+	/* IRQ 3 for UART2, IRQ4 for UART1 */
+	set_irq(dev, SCH5545_LDN_UART1 + uart_no, 4 - uart_no);
+
+	dev = PNP_DEV(port, SCH5545_LDN_UART1 + uart_no);
+	pnp_set_logical_device(dev);
+	pnp_set_enable(dev, 1);
+	pnp_write_config(dev, SCH5545_UART_CONFIG_SELECT,
+			 SCH5545_UART_POWER_VCC);
 
 	pnp_exit_conf_state(dev);
 }
---
+++ src/superio/smsc/sch5545/sch5545_emi.c
@@ -0,0 +1,229 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2019-2020 3mdeb Embedded Systems Consulting
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <arch/io.h>
+#include <device/pnp.h>
+#include <device/pnp_def.h>
+#include <device/pnp_ops.h>
+#include <superio/conf_mode.h>
+
+#include "sch5545.h"
+#include "sch5545_emi.h"
+
+static uint16_t emi_bar;
+
+#ifdef __SIMPLE_DEVICE__
+static void sch5545_enter_conf_state(pnp_devfn_t dev)
+{
+	unsigned port = dev >> 8;
+	outb(0x55, port);
+}
+
+static void sch5545_exit_conf_state(pnp_devfn_t dev)
+{
+	unsigned port = dev >> 8;
+	outb(0xaa, port);
+}
+#endif
+
+uint16_t sch5545_read_emi_bar(uint8_t sio_port)
+{
+	uint16_t bar;
+
+#ifdef __SIMPLE_DEVICE__
+	pnp_devfn_t lpcif = PNP_DEV(sio_port, SCH5545_LDN_LPC_IF);
+	sch5545_enter_conf_state(lpcif);
+#else
+	struct device *lpcif = dev_find_slot_pnp(sio_port, SCH5545_LDN_LPC_IF);
+	if (!lpcif)
+		return 0;
+	pnp_enter_conf_mode_55(lpcif);
+#endif
+	pnp_set_logical_device(lpcif);
+
+	bar = pnp_read_config(lpcif, SCH5545_BAR_EM_IF + 2);
+	bar |= pnp_read_config(lpcif, SCH5545_BAR_EM_IF + 3) << 8;
+
+#ifdef __SIMPLE_DEVICE__
+	sch5545_exit_conf_state(lpcif);
+#else
+	pnp_exit_conf_mode_aa(lpcif);
+#endif
+	return bar;
+}
+
+void sch5545_emi_init(uint8_t sio_port)
+{
+	emi_bar = sch5545_read_emi_bar(sio_port);
+	assert(emi_bar != 0);
+}
+
+void sch5545_emi_ec2h_mailbox_clear(void)
+{
+	sch5545_emi_ec2h_mbox_write(sch5545_emi_ec2h_mbox_read());
+}
+
+void sch5545_emi_disable_interrupts(void)
+{
+	sch5545_emi_set_int_mask(0);
+}
+
+void sch5545_emi_h2ec_mbox_write(uint8_t mbox_message)
+{
+	outb(mbox_message, emi_bar + SCH5545_EMI_HOST_TO_EC_MAILBOX);
+}
+
+uint8_t sch5545_emi_h2ec_mbox_read(void)
+{
+	return inb(emi_bar + SCH5545_EMI_HOST_TO_EC_MAILBOX);
+}
+
+void sch5545_emi_ec2h_mbox_write(uint8_t mbox_message)
+{
+	outb(mbox_message, emi_bar + SCH5545_EMI_EC_TO_HOST_MAILBOX);
+}
+
+uint8_t sch5545_emi_ec2h_mbox_read(void)
+{
+	return inb(emi_bar + SCH5545_EMI_EC_TO_HOST_MAILBOX);
+}
+
+void sch5545_emi_set_int_mask(uint16_t mask)
+{
+	outw(mask, emi_bar + SCH5545_EMI_INT_MASK);
+}
+
+void sch5545_emi_set_int_mask_low(uint8_t mask)
+{
+	outb(mask, emi_bar + SCH5545_EMI_INT_MASK);
+}
+
+void sch5545_emi_set_int_mask_high(uint8_t mask)
+{
+	outb(mask, emi_bar + SCH5545_EMI_INT_MASK + 1);
+}
+
+uint8_t sch5545_emi_get_int_mask_low(void)
+{
+	return inb(emi_bar + SCH5545_EMI_INT_MASK);
+}
+
+uint8_t sch5545_emi_get_int_mask_high(void)
+{
+	return inb(emi_bar + SCH5545_EMI_INT_MASK + 1);
+}
+
+uint16_t sch5545_emi_get_int_mask(void)
+{
+	return inw(emi_bar + SCH5545_EMI_INT_MASK);
+}
+
+void sch5545_emi_set_int_src_low(uint8_t int_src)
+{
+	outb(int_src, emi_bar + SCH5545_EMI_INT_SOURCE);
+}
+
+void sch5545_emi_set_int_src_high(uint8_t int_src)
+{
+	outb(int_src, emi_bar + SCH5545_EMI_INT_SOURCE + 1);
+}
+
+uint8_t sch5545_emi_get_int_src_low(void)
+{
+	return inb(emi_bar + SCH5545_EMI_INT_SOURCE);
+}
+
+uint8_t sch5545_emi_get_int_src_high(void)
+{
+	return inb(emi_bar + SCH5545_EMI_INT_SOURCE + 1);
+}
+uint16_t sch5545_emi_get_int_src(void)
+{
+	return inw(emi_bar + SCH5545_EMI_INT_SOURCE);
+}
+
+void sch5545_emi_set_int_src(uint16_t int_src)
+{
+	outw(int_src, emi_bar + SCH5545_EMI_INT_SOURCE);
+}
+
+void sch5545_emi_set_ec_addr(uint16_t addr)
+{
+	outw(addr, emi_bar + SCH5545_EMI_EC_ADDR);
+}
+
+uint16_t sch5545_emi_read_ec_addr(void)
+{
+	return inw(emi_bar + SCH5545_EMI_EC_ADDR);
+}
+
+void sch5545_emi_ec_write8(uint16_t addr, uint8_t data)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_8BIT_ACCESS);
+	outb(data, emi_bar + SCH5545_EMI_EC_DATA + (addr & 3));
+}
+
+void sch5545_emi_ec_write16(uint16_t addr, uint16_t data)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_16BIT_ACCESS);
+	outw(data, emi_bar + SCH5545_EMI_EC_DATA + (addr & 2));
+}
+
+void sch5545_emi_ec_write32(uint16_t addr, uint32_t data)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_32BIT_ACCESS);
+	outl(data, emi_bar + SCH5545_EMI_EC_DATA);
+}
+
+void sch5545_emi_ec_write32_bulk(uint16_t addr, const uint32_t *data,
+				 size_t len)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_32BIT_AUTO_ACCESS);
+
+	while (len > 0) {
+		outl(*(data++), emi_bar + SCH5545_EMI_EC_DATA);
+		len--;
+	}
+}
+
+uint8_t sch5545_emi_ec_read8(uint16_t addr)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_8BIT_ACCESS);
+	return inb(emi_bar + SCH5545_EMI_EC_DATA + (addr & 3));
+}
+
+uint16_t sch5545_emi_ec_read16(uint16_t addr)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_16BIT_ACCESS);
+	return inw(emi_bar + SCH5545_EMI_EC_DATA + (addr & 2));
+}
+
+uint32_t sch5545_emi_ec_read32(uint16_t addr)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_32BIT_ACCESS);
+	return inb(emi_bar + SCH5545_EMI_EC_DATA);
+}
+
+void sch5545_emi_ec_read32_bulk(uint16_t addr, uint32_t *buffer, size_t len)
+{
+	sch5545_emi_set_ec_addr((addr & 0xfffc) | EMI_EC_32BIT_AUTO_ACCESS);
+
+	while (len > 0) {
+		*(buffer++) = inl(emi_bar + SCH5545_EMI_EC_DATA);
+		len--;
+	}
+}
