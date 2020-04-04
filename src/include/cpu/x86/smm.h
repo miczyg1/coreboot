e8a71c034a1c7f2376420b3955466ff1c2eb4673
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-04-04 21:24:32 +0200

    x86/smm: add serirq and intrusion SMIs
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/include/cpu/x86/smm.h
+++ src/include/cpu/x86/smm.h
@@ -57,6 +57,7 @@ void mainboard_smi_gpi(u32 gpi_sts);
 int  mainboard_smi_apmc(u8 data);
 void mainboard_smi_sleep(u8 slp_typ);
 void mainboard_smi_serirq(void);
+void mainboard_smi_intruder(u32 tco_sts);
 
 /* This is the SMM handler. */
 extern unsigned char _binary_smm_start[];
